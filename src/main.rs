db9ca0da146c08510da3dca145a92ef7ae0aa5d9
Bjoern Ammon <bjoern.ammon1991@gmail.com>
2021-12-04 18:49:49 +0100

    add remove of linebreak

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ fn main() {
     let mut input = String::new();
     match io::stdin().read_line(&mut input){
         Ok(_n) => {
-            match input.as_str() {
-                "1" => println!("You selected the first day!"),
-                _ => println!("Trage eine Zahl von 1 bis 24")
+            let len = input.len();
+            let mut input_value = input.to_string();
+            
+            //remove '\n'
+            input_value.truncate(len - 2);
+            match input_value.as_str() {
+                "1" => print!("Du hast die 1 gewaehlt"),
+                _ => println!("Trage eine Zahl von 1 bis 24 und nicht {0}!", input)
             }
         }
         Err(_error) => println!("{0}", _error)
